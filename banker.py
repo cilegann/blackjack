5e77b520796174423183ce332d06b7fcd537d8d7
cilegann <s571991@gmail.com>
2019-03-02 16:07:23 +0800

    msg ok

--- banker.py
+++ banker.py
@@ -4,8 +4,8 @@ class banker():
     def __init__(self):
         self.bankerHand=[]
         self.playerHand=[]
-        self.bankerHandSum=[]
-        self.playerHandSum=[]
+        self.bankerHandSum='?'
+        self.playerHandSum='?'
         self.playerChip=100
         self.bet=20
         self.stand=False
@@ -49,20 +49,37 @@ class banker():
 
     def getStatusMsg(self):
         m="B["+("X" if self.stand==False else str(self.bankerHand[0]))
-        for i in self.bankerHand[1:]:
-            m+=(","+str(i))
+        for card in self.bankerHand[1:]:
+            m+=(","+str(card) if (card!=1 and card<11) else(",A" if card==1 else(",J" if card==11 else(",Q" if card==12 else ",K"))))
         m+="]"
-        m+=("P["+str(self.playerHand[0]))
-        for i in self.playerHand[1:]:
-            m+=(","+str(i))
+        m+=("P["+(str(self.playerHand[0]) if (self.playerHand[0]!=1 and self.playerHand[0]<11) else("A" if self.playerHand[0]==1 else("J" if self.playerHand[0]==11 else("Q" if self.playerHand[0]==12 else "K")))))
+        for card in self.playerHand[1:]:
+            m+=(","+str(card) if (card!=1 and card<11) else(",A" if card==1 else(",J" if card==11 else(",Q" if card==12 else ",K"))))
         m+="]"
         m+=("C["+str(self.playerChip)+"]")
         m+=("BET["+str(self.bet)+"]")
         return m
 
     def getStatus(self):
-        #TODO: regular message
-        return self.getStatusMsg()
+        m="\n=========================\n"
+        m+="Banker hand:"
+        if not self.stand:
+            m+=" X"
+            for card in self.bankerHand[1:]:
+                m+=(" "+(str(card) if (card!=1 and card<11) else("A" if card==1 else("J" if card==11 else("Q" if card==12 else "K")))))
+        else:
+            for card in self.bankerHand:
+                m+=(" "+(str(card) if (card!=1 and card<11) else("A" if card==1 else("J" if card==11 else("Q" if card==12 else "K")))))
+        
+        m+=("\nBanker sum: "+str(self.bankerHandSum))
+        m+="\nPlayer hand:"
+        for card in self.playerHand:
+            m+=(" "+(str(card) if (card!=1 and card<11) else("A" if card==1 else("J" if card==11 else("Q" if card==12 else "K")))))
+        m+=("\nPlayer sum: "+str(self.playerHandSum))
+        m+=("\nPlayer Remainig chip: "+str(self.playerChip)+"\nBet: "+str(self.bet))
+        m+=("\n"+self.getStatusMsg())
+        m+="\n========================="
+        return m
 
     def calcBankerSum(self):
         #TODO: implement
